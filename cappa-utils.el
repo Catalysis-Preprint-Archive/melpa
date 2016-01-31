4e93dddc7017bd4dd677d509d607e2515265464b
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-31 13:31:19 -0500

    update index and add html pages

--- cappa-utils.el
+++ cappa-utils.el
@@ -1,4 +1,8 @@
 (require 'cl)
+(add-to-list 'load-path "~/Dropbox/kitchingroup/jmax/elpa/s-20140910.334")
+(add-to-list 'load-path "~/Dropbox/kitchingroup/jmax/elpa/dash-20150717.1321")
+(require 's)
+(require 'dash)
 (load-file "package-build.el")
 
 (defun make-index ()
@@ -29,27 +33,17 @@
       (loop for (label . props) in data
 	    do
 	    (insert
-	     (format "<tr>
-<td>%s</td>
-<td>%s</td>
-<td><a href=\"./preprints/%s\">%s</a></td>
+	     (s-format "<tr>
+<td><a href=\"./preprints/$0-$2.$3.html\">$0</a></td>
+<td>$1</td>
 </tr>"
-		     label
-		     (elt props 2)
-		     (if (eq 'single (elt props 3))
-			 (format "%s-%s.%s.el"
-				 label (nth 0 (elt props 0))
-				 (nth 1 (elt props 0)))
-		       (format "%s-%s.%s.tar"
-			       label (nth 0 (elt props 0))
-			       (nth 1 (elt props 0))))
-		     (if (eq 'single (elt props 3))
-			 (format "%s-%s.%s.el"
-				 label (nth 0 (elt props 0))
-				 (nth 1 (elt props 0)))
-		       (format "%s-%s.%s.tar"
-			       label (nth 0 (elt props 0))
-			       (nth 1 (elt props 0)))))))
+		       'elt
+		       (list
+			(symbol-name label)    ;0
+			(elt props 2)	      ;1 description
+			(format "%s" (nth 0 (elt props 0))) ; 2major version
+			(format "%s" (nth 1 (elt props 0)))) ; 3
+		       )))
       (insert "</table>")
 
       (insert "            <script type=\"text/javascript\">
@@ -65,3 +59,75 @@
 
   </body>
 </html>"))))
+
+
+(defun cappa-package-html (key data)
+  "Create an html page for KEY and DATA.
+KEY is :label
+DATA is (plist-get (package-build--archive-alist-for-json) key)."
+  ;; first, gather information.
+  (let* ((package (substring (symbol-name key) 1))
+	 (pkg-el-file (expand-file-name
+		       (format "%s/%s.el" package package)
+		       package-build-working-dir))
+	 (type (plist-get data :type))
+	 (ver (plist-get data :ver))
+	 (pkg-base (format "%s-%s.%s"
+			   package
+			   (nth 0 ver)
+			   (nth 1 ver)))
+	 (pkg-file (format "%s.%s" pkg-base
+			   (if (eq 'single type)
+			       "el"
+			     "tar")))
+	 (html (format "%s/%s.html" "html/packages" pkg-base))
+	 author doi desc commentary)
+
+    (with-current-buffer (find-file-noselect pkg-el-file)
+      (setq author (lm-header "author")
+	    doi (or (lm-header "doi") "")
+	    desc (package-desc-summary (package-buffer-info))
+	    commentary (or (lm-commentary) "")))
+
+    (with-temp-file html
+      (insert
+       (s-format
+     "<html>
+<body>
+
+<h1>${desc}</h1>
+
+${author}<br>
+
+<a href=\"${pkg-file}\">${pkg-file}</a>
+
+<pre>${commentary}</pre>
+
+<pre>
+Contents:
+${contents}
+</pre>
+
+</body></html>"
+
+     'aget
+     `(("desc" . ,desc)
+       ("author" . ,author)
+       ("pkg-file" . ,pkg-file)
+       ("commentary" . ,commentary)
+       ("contents" . ,(if (eq type 'single)
+			  ;; el-file
+			  pkg-file
+			;; tar-file
+			(with-current-buffer
+			    (find-file-noselect
+			     (format "packages/%s" pkg-file))
+			  (buffer-string))))))))))
+
+
+(defun cappa-generate-package-html ()
+  "Generate the package HTML pages."
+  (loop for key in (-slice (package-build--archive-alist-for-json) 0 nil 2)
+	for data in (-slice (package-build--archive-alist-for-json) 1 nil 2)
+	do
+	(cappa-package-html key data)))
